c90a328ae7ea67e8bf76270e60a71dfac9c896f7
shivangirastogi <shivangi.rastogi@adsandurl.com>
2022-05-02 23:44:33 +0530

    lazy loading implemented on the dashboard screen

--- lib/UI/dashboard/dashboard_screen.dart
+++ lib/UI/dashboard/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_assessment_app/models/response/app_response.dart';
 import 'package:get/get.dart';
@@ -11,6 +12,7 @@ class DashboardScreen extends GetWidget<DashboardController> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.teal,
           centerTitle: true,
           elevation: 0,
           leadingWidth: Get.width * 0.2,
@@ -25,18 +27,32 @@ class DashboardScreen extends GetWidget<DashboardController> {
                   itemBuilder: (context, index) {
                     DataList row = controller.dataList[index];
                     return Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.teal.withOpacity(0.1),
+                            spreadRadius: 10,
+                            blurRadius: 5,
+                            offset: const Offset(
+                                0, 8), // changes position of shadow
+                          ),
+                        ],
+                      ),
                       padding: EdgeInsets.symmetric(
                           horizontal: Get.width * .08,
                           vertical: Get.height * .02),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.network(
-                              row.imageHref ??
-                                  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png",
-                              errorBuilder: (context, error, stackTrace) =>
-                                  Image.network(
-                                      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")),
+                          //Loads the images lazily
+                          CachedNetworkImage(
+                            imageUrl: row.imageHref ??
+                                "",
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
                           Text(
                             row.title ?? "No Title",
                             style: const TextStyle(
